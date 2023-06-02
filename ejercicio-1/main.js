45cdf1b7441c1b96cf9f3b7d780e71ae4e8999fe
Fabian Villagra <fabianvillagra28@gmail.com>
2023-06-01 21:09:17 -0300

    Finalizado del primer ejercicio

---
+++ ejercicio-1/main.js
@@ -0,0 +1,8 @@
+let button = document.querySelector("p")
+let second = document.getElementById("boton")
+second.addEventListener("click", function()
+{
+
+    button.classList.toggle("cambiocolor")
+
+})
